8acf3b7839def531a4634c7000262aa9441bed59
Miloni Patel <milonipatel@Milonis-MacBook-Air.local>
2021-07-10 23:56:26 +0530

    heroku deploy

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ request.end();
 app.post("/failure", function(req, res){
     res.redirect("/")
 })
-app.listen("3000", function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("The server is running on port 3000");
 });
 

fc55baf0891f523768ec9b9f50d3e7170bde3068
Miloni Patel <milonipatel@Milonis-MacBook-Air.local>
2021-07-11 00:03:48 +0530

    remove failure page

--- app.js
+++ app.js
@@ -11,46 +11,49 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 });
 
+// app.post("/", function(req, res){
+//     var firstName = req.body.fName;
+//     var lastName = req.body.lName;
+//     var inputEmail = req.body.email;
+
+// const data = {
+//     members:[
+//         {
+//         email_address : inputEmail, 
+//         status: "subscribed",
+//         merge_firlds: {
+//             FNAME: firstName,
+//             LMANE: lastName
+//             }
+//         }
+//     ]
+// }
+// const jsonData = JSON.stringify(data);
+// const url = "https://us6.api.mailchimp.com/3.0/lists/72865584c5";
+// const options = {
+//     method: "POST",
+//     auth: "miloni1:2e9c0a0f507b621707f0646ccbea92a2-us6"
+// }
+// const request=https.request(url, options, function(response){
+// if(response.statusCode == 200){
+//     res.redirect("https://arcane-sands-86747.herokuapp.com/");
+// }else{
+//     res.sendFile(__dirname + "/failure.html");
+// }
+//     response.on("data", function(data){
+//     // console.log(JSON.parse(data));
+//     })
+// })
+// request.write(jsonData);
+// request.end();
+
+// });
+
+// app.post("/failure", function(req, res){
+//     res.redirect("/")
+// })
 app.post("/", function(req, res){
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var inputEmail = req.body.email;
-
-const data = {
-    members:[
-        {
-        email_address : inputEmail, 
-        status: "subscribed",
-        merge_firlds: {
-            FNAME: firstName,
-            LMANE: lastName
-            }
-        }
-    ]
-}
-const jsonData = JSON.stringify(data);
-const url = "https://us6.api.mailchimp.com/3.0/lists/72865584c5";
-const options = {
-    method: "POST",
-    auth: "miloni1:2e9c0a0f507b621707f0646ccbea92a2-us6"
-}
-const request=https.request(url, options, function(response){
-if(response.statusCode == 200){
     res.redirect("https://arcane-sands-86747.herokuapp.com/");
-}else{
-    res.sendFile(__dirname + "/failure.html");
-}
-    response.on("data", function(data){
-    // console.log(JSON.parse(data));
-    })
-})
-request.write(jsonData);
-request.end();
-
-});
-
-app.post("/failure", function(req, res){
-    res.redirect("/")
 })
 app.listen(process.env.PORT || 3000, function(){
     console.log("The server is running on port 3000");
